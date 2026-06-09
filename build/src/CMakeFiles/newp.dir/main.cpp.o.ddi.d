1aaf0256555975e2ffb1425d3a5ebb679ac1141b
Damon Swatton [A10] <damon.swatton@gmail.com>
2026-06-09 12:40:10 +0100

    Unstage build directory.

--- build/src/CMakeFiles/newp.dir/main.cpp.o.ddi.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/newp.dir/main.cpp.o.ddi: \
- /home/damon/Documents/2026/CPP/Microsoft-cpp-programming-essentials/newp/src/main.cpp \
- /usr/include/stdc-predef.h
--- build/src/CMakeFiles/print_hello_world_lib.dir/print_hello_world.cpp.o.ddi.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/print_hello_world_lib.dir/print_hello_world.cpp.o.ddi: \
- /home/damon/Documents/2026/CPP/Microsoft-cpp-programming-essentials/newp/src/print_hello_world.cpp \
- /usr/include/stdc-predef.h
