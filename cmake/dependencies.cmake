24b6cf865ce72eb5f0110c6889d529922a5f3e25
Damon Swatton [A10] <damon.swatton@gmail.com>
2026-06-12 16:22:36 +0100

    Add draft unit tests for Logger class. Not building.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,7 +1,6 @@
-# Add GTest if Debug build
-if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+# Add GTest when tests are enabled.
+if(BUILD_TESTING OR CMAKE_BUILD_TYPE STREQUAL "Debug")
     find_package(GTest REQUIRED)
 endif()
-
 # find_package(yaml-cpp REQUIRED)
 # find_package(ryml REQUIRED)
